49f497cdd011a948026a1696e56371991a71d14d
Aditya <sp.aditya@gmail.com>
2019-10-10 04:23:24 +0530

    Add files via upload

--- progs/bank.py
+++ progs/bank.py
@@ -1,8 +1,14 @@
 # SA -> t = 'S', b cannot be -ve, b = 100
 # CA -> t = 'C', b can be -ve, b = 0
 # DRY --> Dont Repeat Yourself
+
+class InsufficientFundsError(Exception):
+    pass
+
 class Account():
     def __init__(self, n, b, t):
+        if type(self) == Account:
+            raise NameError("Cannot create an object. Use SA or CA")
         self.n = n
         self.b = b
         self.t = t
@@ -16,7 +22,7 @@ class SA(Account):
         Account.__init__(self, n, b, 'S')
     def debit(self, amount):
         if self.b < amount:
-            print("Insufficient Balance")
+            raise InsufficientFundsError("Insufficient Funds")
         else :
             Account.debit(self,amount)
 class CA(Account):
--- progs/transactions.py
+++ progs/transactions.py
@@ -5,7 +5,10 @@ sa = bank.SA('Aditya')
 print(sa.n, sa.b, sa.t)
 sa.credit(100)
 print(sa.n, sa.b, sa.t)
-sa.debit(2000)
+try:
+    sa.debit(2000)
+except bank.InsufficientFundsError as err:
+    print("Please check your balance and try again!")
 
 ca = bank.CA('ABC Inc')
 print(ca.n, ca.b, ca.t)
@@ -13,3 +16,5 @@ ca.credit(100)
 print(ca.n, ca.b, ca.t)
 ca.debit(2000)
 print(ca.n, ca.b, ca.t)
+
+#ac1 = bank.Account("A", 100, 'c')
