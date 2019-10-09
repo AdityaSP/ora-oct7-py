05cfee8e7ecf917bf69d0de87f5e39a7ec20cef9
Aditya <sp.aditya@gmail.com>
2019-10-10 02:55:38 +0530

    Add files via upload

--- progs/bank.py
+++ progs/bank.py
@@ -1,8 +1,23 @@
-class Account():
-    def __init__(self, n, b, t):
+# SA -> t = 'S', b cannot be -ve, b = 100
+# CA -> t = 'C', b can be -ve, b = 0
+# DRY --> Dont Repeat Yourself
+class SA():
+    def __init__(self, n, b=100):
         self.n = n
         self.b = b
-        self.t = t
+        self.t = 'S'
+    def credit(self, amount):
+        self.b += amount
+    def debit(self, amount):
+        if self.b < amount:
+            print("Insufficient Balance")
+        else :
+            self.b -= amount
+class CA():
+    def __init__(self, n, b=0):
+        self.n = n
+        self.b = b
+        self.t = 'C'
     def credit(self, amount):
         self.b += amount
     def debit(self, amount):
