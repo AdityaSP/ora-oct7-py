751f3d9be1aeb7f94408dfa2e8ab02d02326222a
Aditya <sp.aditya@gmail.com>
2019-10-11 22:39:41 +0530

    Add files via upload

--- progs/posterdownload.py
+++ progs/posterdownload.py
@@ -3,16 +3,24 @@
 
 import requests
 import json
+from threading import Thread
+
 ui= input("Movie search string: ")
 url = r'http://www.omdbapi.com/?s='+ ui + r'&apikey=b4e17ea0'
 r = requests.get(url)
+
+def download(posterurl, filename):
+    poster = requests.get(posterurl)
+    if poster.ok:
+        fh = open(filename, 'wb')
+        fh.write(poster.content)
+        fh.close()    
+
 if r.ok:
     data = json.loads(r.text)
     for movie in data['Search']:
         print("Downloading for", movie['Title'])
         if movie['Poster'] != 'N/A':
-            poster = requests.get(movie['Poster'])
-            if poster.ok:
-                fh = open(movie['imdbID'] + '.jpg', 'wb')
-                fh.write(poster.content)
-                fh.close()
+            #download(movie['Poster'] , movie['imdbID'] + '.jpg')
+            Thread(target=download, args=[movie['Poster'] , movie['imdbID'] + '.jpg']).start()
+            
